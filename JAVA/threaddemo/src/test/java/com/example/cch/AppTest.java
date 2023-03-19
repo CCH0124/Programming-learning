358a1be2f3b3bcb0b7709a0095b3a8195a82218d
CCH0124 <cchong0124@gmail.com>
2023-03-19 17:29:10 +0800

    add gitignore

--- JAVA/threaddemo/src/test/java/com/example/cch/AppTest.java
+++ JAVA/threaddemo/src/test/java/com/example/cch/AppTest.java
@@ -17,4 +17,34 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    
+    class Person {
+        int age;
+        String name;
+
+        @Override
+        public String toString() {
+            // TODO Auto-generated method stub
+            return super.toString();
+        }
+    }
+    public void modify(int i, String s, Person p) {
+        i = i * 2;
+        s = "Goodbye";
+        p.name = "Bob";
+        
+    }
+    @Test
+    public void modifyTest() {
+        Person p = new Person();
+        int i = 2;
+        String s = "Hello";
+        modify(i, s, p);
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(p.age);
+        System.out.println(p.name);
+
+    }
 }
